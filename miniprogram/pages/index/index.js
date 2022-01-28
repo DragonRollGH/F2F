2034b0f562735b282edd120921015c50219552ff
DragonRollGH <492782447@qq.com>
2022-01-28 18:18:39 +0800

    220128

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -94,26 +94,55 @@ function initPage() {
  
 function mainBtn() {
   page.setData({
-    mainBtnTxt: "0时0分0秒"
+    mainBtnTxt: "0分0秒"
   });
   isF2F ? checkout() : checkin();
 }
 
 function onLoad() {
   page = this;
+  tb.get({
+    success: res => {
+      console.log(res.data);
+      let timeTable = [];
+      for (let t in res.data) {
+        if (res.data[t].ST && res.data[t].ET) {
+          timeTable.unshift({
+            DT: res.data[t].ST.toLocaleDateString(),
+            ST: res.data[t].ST.toLocaleTimeString(),
+            ET: res.data[t].ET.toLocaleTimeString(),
+          });
+        }
+      }
+      page.setData({
+        timeTable: timeTable,
+      })
+    }
+  })
   loadDatabase(initPage);
   setInterval(() => {
-    let now = new Date(new Date() - timeStamp);
-    page.setData({
-      mainBtnTxt: `${now.getHours()-8}时${now.getMinutes()}分${now.getSeconds()}秒`
-    });
+    let nows = new Date() - timeStamp;
+    let now = new Date(nows - 8*3600000); 
+    if(nows < 3600000) {  // 1 hour
+      page.setData({
+        mainBtnTxt: `${now.getMinutes()}分${now.getSeconds()}秒`
+      });
+    } else if (nows < 24*3600000) {  // 1 day
+      page.setData({
+        mainBtnTxt: `${now.getHours()}时${now.getMinutes()}分`
+      });
+    } else {
+      page.setData({
+        mainBtnTxt: `${Math.floor(now / (24*3600000))}天${now.getHours()}时`
+      });
+    }
   },1000);
 }
 
 
 Page({
   data:{
-    mainBtnTxt: "0时0分0秒",
+    mainBtnTxt: "0分0秒",
     mainBtnClr: "white",
     ST: "&nbsp;",
     ET: "&nbsp;",
